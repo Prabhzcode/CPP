7b531484cb2c8b064927a80f3b73c88d699d8458
Prabhzcode <prabhhere638@gmail.com>
2025-08-29 00:15:50 +0530

    10 patterns

--- Pattern_01.cpp
+++ Pattern_01.cpp
@@ -91,13 +91,52 @@ void pattern8(int n){
     }
 }
 
+void pattern9(int n){//pattter 7 and 8 can be called combined for this
+    for (int i=0 ; i<n/2 ; i++){
+        for ( int j = 0 ; j<(n/2-i)-1 ; j++){
+            cout << "  ";
+        }
+        for(int j =0 ; j <2*(i+1)-1 ; j++ ){
+            cout << "* ";
+        }
+        for( int j = 0 ; j<(n/2-i)-1 ; j++ ){
+            cout << "  ";
+        }
+        cout << endl ;
+    }
+
+    for (int i=n/2 ; i<n ; i++){
+        for ( int j = 0 ; j<(i-n/2) ; j++){
+            cout << "  ";
+        }
+        for(int j =0 ; j <2*(n-i)-1 ; j++ ){
+            cout << "* ";
+        }
+        for( int j = 0 ; j<(i-n/2) ; j++ ){
+            cout << "  ";
+        }
+        cout << endl ;
+    }
+}
+
+void pattern10(int n){
+    for(int i= 1; i<=2*n-1;i++){
+        int star = i;
+        if (i>n) star = 2*n -1;
+        for(int j=1 ; j<=star ; j++){
+            cout << "* " ;
+        }
+        cout << endl;
+    }
+}
+
 int main(){
     int t ;//for test cases 
     cin >> t ;
     for (int i = 0 ; i<t;i++){
         int n;
         cin >> n;
-        pattern8(n);
+        pattern10(n);
 
     }
 }
