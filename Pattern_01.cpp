53b18834be259982e258eec5becfa924571cfce3
Prabhzcode <prabhhere638@gmail.com>
2025-08-31 18:30:26 +0530

    20

--- Pattern_01.cpp
+++ Pattern_01.cpp
@@ -122,7 +122,7 @@ void pattern9(int n){//pattter 7 and 8 can be called combined for this
 void pattern10(int n){
     for(int i= 1; i<=2*n-1;i++){
         int star = i;
-        if (i>n) star = 2*n -1;
+        if (i>n) star = 2*n -i;
         for(int j=1 ; j<=star ; j++){
             cout << "* " ;
         }
@@ -130,13 +130,152 @@ void pattern10(int n){
     }
 }
 
+void pattern11(int n){
+    int start = 1;
+    for (int i = 1 ; i <= n ; i++ ){
+        if( i%2 == 0) start = 0;
+        else start = 1; 
+        for (int j = 1 ; j<=i; j++){
+            cout << start << " "  ;
+            start =1 - start ;//MATHEMATICAL TOGGLE
+        }
+        cout << endl;
+    }
+}
+
+void pattern12(int n){
+    for(int i = 1 ; i <=n ; i++ ){
+        for(int j = 1 ; j <= i ; j ++){
+            cout  << j << " ";
+        }  
+        for(int j = 1 ; j <= 2*(n-i) ; j ++){
+            cout  << "  ";
+        }
+        for(int j = i ; j >= 1 ; j --){
+            cout  << j << " ";
+        }
+        cout << endl ;
+    }
+}
+
+void pattern13(int n){
+    int start = 1;
+    for (int i = 1 ; i <= n ; i++ ){
+        for (int j = 1 ; j<=i; j++){
+            cout << start << " "  ;
+            start +=1;
+        }
+        cout << endl;
+    }
+}
+
+void pattern14(int n ){
+    for (int i = 0 ; i < n ; i++){
+        for (char ch= 'A' ; ch <= 'A' + i ; ch++){
+            cout << ch << " " ;
+        }
+        cout << endl;
+    }
+}
+
+void pattern15(int n ){
+    for (int i = 1 ; i <= n ; i++){
+        for (char ch= 'A' ; ch <= 'A' + n-i ; ch++){
+            cout << ch << " " ;
+        }
+        cout << endl;
+    }
+}
+
+void pattern16(int n ){
+    for (int i = 0 ; i < n ; i++){
+        char ch = 'A' + i;
+        for (int j = 0 ; j <= i; j++){
+            cout << ch << " " ;
+        }
+        cout << endl;
+    }
+}
+
+void pattern17(int n){
+    for (int i = 0 ; i < n ; i++ ){
+        char ch1 = 'A' + i;
+        for(int j = 0 ; j < n-i ; j ++){
+            cout << "  ";
+        } 
+        for (char ch = 'A' ; ch <= 'A'+ i ; ch++){
+            cout << ch << " ";
+        }
+        for(char ch = 'A' + i-1 ; ch >= 'A' ; ch--){
+            cout << ch << " " ;
+        }
+        for(int j = 0 ; j < n-i ; j ++){
+            cout << "  ";
+        }
+        cout << endl ; 
+    }
+}
+
+void pattern18(int n){
+    for (int i = 0 ; i <n ; i++){
+        for (char j = 'A' + n-i-1; j < 'A'+ n ; j++){
+            cout << j << " ";
+        }
+        cout << endl ;
+    }
+}
+
+void pattern19(int n ){
+    for (int i = 0 ; i<n ; i++){
+        int flip_1 = n/2-i;
+        int flip_2 = 2*i;
+        if (i >= n/2) {
+            flip_1 = i-n/2+1;
+            flip_2 = 2*(n-i-1); 
+        }
+        for ( int j = 0 ; j < flip_1 ; j ++){
+            cout << "* ";
+        }
+        for (int j = 0 ; j < flip_2 ; j ++){
+            cout << "  ";
+        }
+              for ( int j = 0 ; j < flip_1 ; j ++){
+            cout << "* ";
+        }
+        cout << endl ; 
+    }
+}
+
+void pattern20(int n ){
+    for (int i = 0 ; i<n ; i++){
+        int flip_1 = i+1;
+        int flip_2 = n-(2*i+1);
+        if (i > n/2) {
+            flip_1 = n-i;
+            flip_2 = 2*i-n+1; 
+        }
+        for ( int j = 0 ; j < flip_1 ; j ++){
+            cout << "*";
+        }
+        for (int j = 0 ; j < flip_2 ; j ++){
+            cout << " ";
+        }
+              for ( int j = 0 ; j < flip_1 ; j ++){
+            cout << "*";
+        }
+        cout << endl ; 
+    }
+}
+
+
+
 int main(){
     int t ;//for test cases 
     cin >> t ;
     for (int i = 0 ; i<t;i++){
         int n;
         cin >> n;
-        pattern10(n);
+        pattern20(n);
 
     }
 }
